6e888d832f05de9d1a6270542908b305b28a6739
Sean Drewry <seanmdrewry@gmail.com>
2024-01-10 16:54:43 +0000

    CSS styling and formatting HTML files (beautiful buttons) (you will see what we mean)

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,10 +1,10 @@
 from lib.user import User
 
 def test_user_constructs():
-    user = User(1, 'email@gmail.com')
+    user = User(1, 'name', 'hello@gmail.com', 'Password')
     assert user.id == 1
-    assert user.email == 'email@gmail.com'
+    assert user.email == 'hello@gmail.com'
 
 def test_format():
-    user = User(1, 'email@gmail.com')
-    assert str(user) == "user(1, 'email@email.com')"
+    user = User(1, 'name', 'hello@gmail.com', 'Password')
+    assert str(user) == "User(1, 'name', 'hello@gmail.com', 'Password')"
